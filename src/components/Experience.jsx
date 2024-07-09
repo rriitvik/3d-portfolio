73c2ba95412714c5753674de84945264afe9754c
Ritvik <gopalkaritvik117@gmail.com>
2024-07-09 18:19:06 +0530

    final change

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,7 +11,7 @@ const ExperienceCard = ({experience}) => (
   <VerticalTimelineElement
   contentStyle={{ background: '#1d1836', color: '#fff' }}
   contentArrowStyle={{ borderRight: '7px solid  #232631' }}
-  date={experience.date}
+  date={experience.company_name}
   iconStyle={{ background: experience.iconBg }}
   icon={
     <div className="flex justify-center items-center w-full h-full">
@@ -27,9 +27,7 @@ const ExperienceCard = ({experience}) => (
       <h3 className="text-white text-[24px] font-bold">
         {experience.title}
       </h3>
-      <p className="text-secondary text-[16px] font-semibold" style={{margin:0}}>
-        {experience.company_name}
-      </p>
+      
     </div>
     <ul className="mt-5 list-disc ml-5 space-y-2">
       {experience.points.map((point, index) => (
